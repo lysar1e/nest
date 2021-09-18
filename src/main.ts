7798e8dfaed352723715b9eae8bca7f03a8c6388
lysar1e <79013918+lysar1e@users.noreply.github.com>
2021-09-19 03:38:48 +0600

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -7,6 +7,6 @@ async function bootstrap() {
   app.use(cookieParser());
   app.enableCors({ origin: "http://fasfafsa.fun", credentials: true });
   app.setGlobalPrefix("api");
-  await app.listen(process.env.PORT || 5000);
+  await app.listen(process.env.PORT || 8080);
 }
 bootstrap();
